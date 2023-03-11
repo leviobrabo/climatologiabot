794873d5583e644513d0db0761742784de5c7d88
carlosjunior <carlosjunior20313@gmail.com>
2023-03-11 00:44:26 -0300

    add "name" em database

--- src/database/models/users.js
+++ src/database/models/users.js
@@ -4,7 +4,8 @@ const UserSchema = new Schema({
   firstName: { type: String },
   userID: { type: Number, required: true },
   username: { type: String },
-  city: { type: String, required: false }
+  city: { type: String, required: false },
+  name: { type: String, required: false }
 });
 
 module.exports = UserSchema;
