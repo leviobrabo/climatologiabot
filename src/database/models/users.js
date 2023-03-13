63e6e569e1d4bf34d9a66cb7ab63631afe6f4211
carlosjunior <carlosjunior20313@gmail.com>
2023-03-13 01:35:42 -0300

    Update models database + inlineSearch

--- src/database/models/users.js
+++ src/database/models/users.js
@@ -6,7 +6,8 @@ const UserSchema = new Schema({
   username: { type: String },
   city: { type: String, required: false },
   name: { type: String, required: false },
-  lang: { type: String, default: 'en' } // adiciona um campo de idioma padrão
+  lang: { type: String, default: 'en' }, // adiciona um campo de idioma padrão
+  inlineSearch: { type: String, required: false } // adiciona um novo campo para armazenar a pesquisa do usuário
 });
 
 module.exports = UserSchema;
