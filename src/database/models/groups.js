1b1167cfe34641e38ae3f3d3ba250f84d8c09b88
carlosjunior <carlosjunior20313@gmail.com>
2023-03-18 11:47:35 -0300

    Adicionado o modelo para groups

---
+++ src/database/models/groups.js
@@ -0,0 +1,15 @@
+const {Schema} = require('mongoose')
+
+const ChatSchema = new Schema({
+  chatId: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  chatName: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = ChatSchema
