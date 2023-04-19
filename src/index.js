1de7602160a09109e64f364550172530283551fb
leviobrabo <carlosjunior20313@gmail.com>
2023-04-19 17:32:22 -0300

    upulado 1.3.7

--- src/index.js
+++ src/index.js
@@ -861,3 +861,79 @@ bot.onText(/^(\/broadcast|\/bc)\b/, async (msg, match) => {
         }
     );
 });
+
+bot.onText(/\/dev/, async (msg) => {
+    const userId = message.from.id;
+    if (message.chat.type !== "private") {
+        return;
+    }
+    const firstName = message.from.first_name;
+    const message_start_dev = `Olá, <b>${firstName}</b>! Você é um dos desenvolvedores 🧑‍💻\n\nVocê está no painel do desenvolvedor da Janna, então aproveite a responsabilidade e use os comandos com consciências`;
+    const options_start_dev = {
+        parse_mode: "HTML",
+        disable_web_page_preview: true,
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: "📬 Canal Oficial",
+                        url: "https://t.me/climatologiaofc",
+                    },
+                ],
+                [
+                    {
+                        text: "🗃 Lista de para desenvolvedores",
+                        callback_data: "commands",
+                    },
+                ],
+            ],
+        },
+    };
+    bot.on("callback_query", async (callbackQuery) => {
+        if (callbackQuery.message.chat.type !== "private") {
+            return;
+        }
+        const chatId = callbackQuery.message.chat.id;
+        const messageId = callbackQuery.message.message_id;
+
+        if (callbackQuery.data === "commands") {
+            const commands = [
+                "/stats - Estatística de grupos, usuarios e mensagens enviadas",
+                "/broadcast ou /bc - envia mensagem para todos usuários",
+                "/ping - veja a latência da VPS",
+            ];
+            await bot.editMessageText(
+                "<b>Lista de Comandos:</b> \n\n" + commands.join("\n"),
+                {
+                    parse_mode: "HTML",
+                    disable_web_page_preview: true,
+                    chat_id: chatId,
+                    message_id: messageId,
+                    reply_markup: {
+                        inline_keyboard: [
+                            [
+                                {
+                                    text: "⬅️ Voltar",
+                                    callback_data: "back_to_start",
+                                },
+                            ],
+                        ],
+                    },
+                }
+            );
+        } else if (callbackQuery.data === "back_to_start") {
+            await bot.editMessageText(message_start_dev, {
+                parse_mode: "HTML",
+                chat_id: chatId,
+                message_id: messageId,
+                disable_web_page_preview: true,
+                reply_markup: options_start_dev.reply_markup,
+            });
+        }
+    });
+    if (is_dev(userId)) {
+        bot.sendMessage(userId, message_start_dev, options_start_dev);
+    } else {
+        bot.sendMessage(message.chat.id, "Você não é desenvolvedor");
+    }
+});
