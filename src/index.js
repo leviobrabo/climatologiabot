0307ca9ed5105751fb27473923206ec51cb04299
leviobrabo <carlosjunior20313@gmail.com>
2023-04-21 20:01:14 -0300

    update

--- src/index.js
+++ src/index.js
@@ -727,22 +727,24 @@ bot.on("new_chat_members", async (msg) => {
         console.log(
             `Grupo ${chat.chatName} (${chat.chatId}) adicionado ao banco de dados`
         );
-        const message = `#Climatologiabot #New_Group
-    <b>Group:</b> <a href="tg://resolve?domain=${chat.chatName}&amp;id=${chat.chatId}">${chat.chatName}</a>
-    <b>ID:</b> <code>${chat.chatId}</code>`;
-        bot.sendMessage(groupId, message, { parse_mode: "HTML" }).catch(
-            (error) => {
-                console.error(
-                    `Erro ao enviar mensagem para o grupo ${groupId}: ${error}`
-                );
-            }
-        );
+
         const botUser = await bot.getMe();
         const newMembers = msg.new_chat_members.filter(
             (member) => member.id === botUser.id
         );
 
         if (newMembers.length > 0) {
+            const message = `#Climatologiabot #New_Group
+            <b>Group:</b> <a href="tg://resolve?domain=${chat.chatName}&amp;id=${chat.chatId}">${chat.chatName}</a>
+            <b>ID:</b> <code>${chat.chatId}</code>`;
+            bot.sendMessage(groupId, message, { parse_mode: "HTML" }).catch(
+                (error) => {
+                    console.error(
+                        `Erro ao enviar mensagem para o grupo ${groupId}: ${error}`
+                    );
+                }
+            );
+
             bot.sendMessage(
                 chatId,
                 "(🇧🇷) Olá, meu nome é Janna! Obrigado por me adicionado em seu grupo. Eu sou bot de previsão do tempo, para usar digite @climatologiabot cidade.\n\n(🇺🇸)Hello, my name is Janna! Thanks for adding me to your group. I'm a weather forecast bot, to use type @climatologiabot city.",
